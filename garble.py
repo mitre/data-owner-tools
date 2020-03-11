0ce07e77d935c7c5308ca71ee665ea4543a3dbab
Andy Gregorowicz <andrewg@mitre.org>
2020-03-11 10:23:32 -0400

    Fixes for Windows

--- garble.py
+++ garble.py
@@ -2,37 +2,35 @@ import os
 import sys
 import getpass
 from zipfile import ZipFile
+from pathlib import Path
 import argparse
 
 parser = argparse.ArgumentParser(description='Tool for garbling PII in for PPRL purposes in the CODI project')
 parser.add_argument('--source', nargs=1, required=True, help='Source PII CSV file')
 parser.add_argument('--schema', nargs=1, required=True, help='Directory of linkage schema')
 args = parser.parse_args()
-command = "clkutil hash {source_file} {secret_one} {secret_two} {schema_path} {output_file}"
+command = "clkutil hash {source_file} {secret_one} {schema_path} {output_file}"
 
-schema_dir = args.schema[0]
+schema_dir = Path(args.schema[0])
 
-if not schema_dir.endswith('/'):
-  schema_dir = schema_dir + '/'
-
-if not os.path.exists(schema_dir):
+if not schema_dir.exists():
   sys.exit('Unable to find directory: ' + schema_dir)
 
 schema = filter(lambda f: f.endswith('.json'), os.listdir(schema_dir))
 
 source_file = args.source[0]
-secret_one = getpass.getpass('First salt value: ')
-secret_two = getpass.getpass('Second salt value: ')
+secret_one = getpass.getpass('Salt value: ')
 clk_files = []
 
 if not os.path.exists('output'):
   os.mkdir('output')
 
 for s in schema:
-  schema_path = schema_dir + s
-  output_file = 'output/' + s
+  schema_path = schema_dir.joinpath(s)
+  output_file = Path('output', s)
   to_execute = command.format(source_file=source_file, secret_one=secret_one,
-    secret_two=secret_two, schema_path=schema_path, output_file=output_file)
+    schema_path=schema_path, output_file=output_file)
+  print(to_execute)
   os.system(to_execute)
   clk_files.append(output_file)
 
