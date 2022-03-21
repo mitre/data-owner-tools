d599d5bd4ac785fc66a3daab0aedfb702e0c853a
Dylan Hall <dehall@mitre.org>
2022-03-21 17:28:12 -0400

    more configurability on analysis script

--- data_analysis.py
+++ data_analysis.py
@@ -18,6 +18,24 @@ def parse_args():
         "--db",
         help='Connection string for DB to analyze',
     )
+
+    # DB-related arguments
+    parser.add_argument(
+        '--schema',
+        default='codi',
+        help="Database schema to read from. Default is 'codi'"
+    )
+    parser.add_argument(
+        '--table',
+        default='identifier',
+        help="Database table or view to read from. Default is 'identifier'"
+    )
+    parser.add_argument(
+        '--idcolumn',
+        default='patid',
+        help="Column name for patient unique ID. Default is 'patid'"
+    )
+
     args = parser.parse_args()
     return args
 
@@ -35,27 +53,31 @@ DATA_DICTIONARY = {
 }
 
 
-def load_db(connection_string):
-    db_data = pd.read_sql_query('select * from codi.identifier', connection_string)
+def get_column(row, key, source):
+    desired_key = DATA_DICTIONARY[key][source]
+
+    if desired_key in row:
+        return row[desired_key]
+    else:
+        for actual_key in row.keys():
+            if actual_key.lower() == desired_key:
+                return row[actual_key]
+
+
+def load_db(args):
+    connection_string = args.db
+    schema = args.schema
+    table = args.table
+    # This looks like SQL injection, but since it requires the user
+    # to pass in their own credentials, there's no risk of privilege escalation
+    # or uninintended data being returned.
+    db_data = pd.read_sql_query(f"select * from {schema}.{table}", connection_string)
     return db_data  
 
 
 def load_csv(filepath):
-    all_strings = {
-        'record_id': 'str',
-        'given_name': 'str',
-        'family_name': 'str',
-        'DOB': 'str',
-        'sex': 'str',
-        'phone_number': 'str',
-        'household_street_address': 'str',
-        'household_zip': 'str',
-        'parent_given_name': 'str',
-        'parent_family_name': 'str',
-        'parent_email': 'str'
-    }
     # force all columns to be strings, even if they look numeric
-    csv_data = pd.read_csv(filepath, dtype=all_strings)
+    csv_data = pd.read_csv(filepath, dtype=str)
     return csv_data
 
 
@@ -65,8 +87,8 @@ def analyze(data, source):
 
     stats['number_of_rows'] = len(data.index)
 
-    patid_col = DATA_DICTIONARY['patid'][source]
-    patid_stats = top_N(data, patid_col)
+    patid_col = get_column(data, 'patid', source)
+    patid_stats = top_N(patid_col)
     stats['total_unique_patids'] = len(patid_stats)
 
     patid_dups = {k:v for (k,v) in patid_stats.items() if v > 1}
@@ -75,13 +97,12 @@ def analyze(data, source):
         # only report individual IDs if there are less than 20% dups
         raw_values['duplicate_patids'] = patid_dups
 
-    dob_col = DATA_DICTIONARY['dob'][source]
-    dob_values = data[dob_col]
-    notnull_dobs = dob_values.dropna()
+    dob_col = get_column(data, 'dob', source)
+    notnull_dobs = dob_col.dropna()
     stats['dob'] = {
         'min': str(notnull_dobs.min()),  # str-ify because we get Timestamp from DB
         'max': str(notnull_dobs.max()),  # which is not serializable
-        'missing': int(dob_values.isna().sum())
+        'missing': int(dob_col.isna().sum())
     }
 
     expected_min_dob = '1997-01-01'  # roughly, 19 years old at the start of CODI observation period (2016)
@@ -117,36 +138,37 @@ def analyze(data, source):
         out_of_range = notnull_dobs[notnull_dobs < expected_min_dob]
         stats['dob']['count_earlier_dob_than_expected'] = len(out_of_range)
 
-    sex_col = DATA_DICTIONARY['sex'][source]
-    stats['sex'] = top_N(data, sex_col)
+    sex_col = get_column(data, 'sex', source)
+    stats['sex'] = top_N(sex_col)
 
-    zip_col = DATA_DICTIONARY['zip'][source]
-    data['zip_format'] = data[zip_col].map(to_format)
-    stats['zip_format'] = top_N(data, 'zip_format')
+    zip_col = get_column(data, 'zip', source)
+    data['zip_format'] = zip_col.map(to_format)
+    stats['zip_format'] = top_N(data['zip_format'])
 
-    stats['top_10_zip_codes'] = top_N(data, zip_col, 10)
+    stats['top_10_zip_codes'] = top_N(zip_col, 10)
 
-    phone_col = DATA_DICTIONARY['phone'][source]
-    data['phone_format'] = data[phone_col].map(to_format)
-    stats['phone_format'] = top_N(data, 'phone_format')
+    phone_col = get_column(data, 'phone', source)
+    data['phone_format'] = phone_col.map(to_format)
+
+    stats['phone_format'] = top_N(data['phone_format'])
 
     # Note: the following top 10s are limited to anything that occurs 3+ times,
     # in order to limit the potential spill of PII.
     # For this analysis, any names that appear 2 or fewer times are not especially useful.
-    given_name_col = DATA_DICTIONARY['given_name'][source]
-    raw_values['top_10_given_names'] = top_N(data, given_name_col, 10, 3)
+    given_name_col = get_column(data, 'given_name', source)
+    raw_values['top_10_given_names'] = top_N(given_name_col, 10, 3)
 
-    family_name_col = DATA_DICTIONARY['family_name'][source]
-    raw_values['top_10_family_names'] = top_N(data, family_name_col, 10, 3)
+    family_name_col = get_column(data, 'family_name', source)
+    raw_values['top_10_family_names'] = top_N(family_name_col, 10, 3)
 
-    address_col = DATA_DICTIONARY['address'][source]
-    raw_values['top_10_addresses'] = top_N(data, address_col, 10, 3)
+    address_col = get_column(data, 'address', source)
+    raw_values['top_10_addresses'] = top_N(address_col, 10, 3)
 
-    raw_values['top_10_phone_numbers'] = top_N(data, phone_col, 10, 3)
+    raw_values['top_10_phone_numbers'] = top_N(phone_col, 10, 3)
 
     stats['field_summaries'] = {}
     for col in [given_name_col, family_name_col, address_col, zip_col, phone_col]:
-        stats['field_summaries'][col] = summary(data, col)
+        stats['field_summaries'][col.name] = summary(col)
 
     # TODO: possible quick win: look at the year/month/day breakdown in dates
 
@@ -175,11 +197,11 @@ def to_format(numeric_string):
     return no_letters
 
 
-def top_N(df, column, N=0, lower_limit=1):
+def top_N(series, N=0, lower_limit=1):
     # return a dict of the top N most prevalent entries in the column
     # and the count of each
 
-    top_n = df[column].value_counts()
+    top_n = series.value_counts()
 
     if N > 0:
         top_n = top_n[:N]
@@ -194,9 +216,7 @@ def top_N(df, column, N=0, lower_limit=1):
     return top_n
 
 
-def summary(df, column):
-    series = df[column]
-
+def summary(series):
     # 1. count the number of missing (null) entries
     missing = series.isna().sum()
 
@@ -226,7 +246,8 @@ if __name__ == "__main__":
         source = 'csv'
 
     if args.db:
-        db_data = load_db(args.db)
+        DATA_DICTIONARY['patid']['db'] = args.idcolumn
+        db_data = load_db(args)
         results = analyze(db_data, 'db')
         source = 'db'
 
