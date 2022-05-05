8205a759807d470e2341df683c994e13cc8b829f
radamson <radamson@users.noreply.github.com>
2022-05-05 19:12:00 -0400

    fix build_key.py flake8 issues

--- testing-and-tuning/build_key.py
+++ testing-and-tuning/build_key.py
@@ -2,10 +2,12 @@ import csv
 import json
 from pathlib import Path
 
-# This script creates the site-specific answer key CSVs out of the overall answer_key JSON.
+# This script creates the site-specific answer key CSVs out of the
+# overall answer_key JSON.
 # It iterates over the json and breaks out people based on which site they belong to.
 # The columns of the CSV are just the 4 fields from the objects in the JSON.
-# Note that nothing actually uses the file_name so it could be stripped for file size if necessary.
+# Note that nothing actually uses the file_name so it could be stripped
+# for file size if necessary.
 
 answer_key = Path("../temp-data/answer_key.json")
 
@@ -32,19 +34,19 @@ with open(answer_key) as f:
     #       "record_id": "19029",
     #       "seed_record_id": "19028",
     #       "household_id": "3879064",
-    #       "file_name": "Andrew_Nikla_Denbraber_14444032-081e-92ac-47dd-eafdbce663652.json"
+    #       "file_name": "Andrew_Nikla_Denbraber.json"
     #     },
     #     {
     #       "record_id": "19030",
     #       "seed_record_id": "19028",
     #       "household_id": "3879064",
-    #       "file_name": "Dr_Andrew_Denbraber_14444032-081e-92ac-47dd-eafdbce663650.json"
+    #       "file_name": "Dr_Andrew_Denbraber.json"
     #     },
     #     {
     #       "record_id": "19029",
     #       "seed_record_id": "19028",
     #       "household_id": "3879064",
-    #       "file_name": "Andrew_Nikla_Denbraber_14444032-081e-92ac-47dd-eafdbce663651.json"
+    #       "file_name": "Andrew_Nikla_Denbraber.json"
     #     }
     #   ],
 
