734d82499ec84d010e7e212793c76c40ad269952
Dylan Hall <dehall@mitre.org>
2023-03-14 13:09:56 -0400

    add split_factor arg

--- households.py
+++ households.py
@@ -68,6 +68,14 @@ def parse_arguments():
         action="store_true",
         help="Optional generate files used for testing against an answer key",
     )
+    parser.add_argument(
+        "--split_factor",
+        type=int,
+        default=4,
+        help="Number of segments to split data into when inferring households."
+        " Smaller numbers may result in out of memory errors. Larger numbers"
+        " may increase runtime. Default is 4",
+    )
     args = parser.parse_args()
     if args.sourcefile and not Path(args.sourcefile).exists():
         parser.error("Unable to find source file: " + args.secretfile)
@@ -186,7 +194,7 @@ def write_mapping_file(pos_pid_rows, hid_pat_id_rows, args):
         # (patient position) --> [matching pairs that include that patient]
         # so it can be traversed sort of like a graph from any given patient
         # note the key is patient position within the pii_lines dataframe
-        pos_to_pairs = get_houshold_matches(pii_lines)
+        pos_to_pairs = get_houshold_matches(pii_lines, args.split_factor)
 
         hclk_position = 0
         # Match households
